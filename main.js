73435de42f797696ba8473083c6cd94d40672f2b
Kevin Edin <kevin.edin@elev.ga.ntig.se>
2025-11-17 11:05:15 +0100

    gameloop, stop knapp, enemy class

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 const playerName = "Gunnar"
-const enemyName = "Bertil"
 let playerHp = 100
-let enemyHp = 100
 
-const startButton = document.querySelector("#start-button")
-startButton.addEventListener("click", startRound)
-
-const playerHpText = document.querySelector("#player-hp")
-playerHpText.textContent = playerHp
-
-const enemyHpText = document.querySelector("#enemy-hp")
-enemyHpText.textContent = enemyHp
-
-const combatLog = document.querySelector("#combat-log")
+let currentEnemy
+class Enemy {
+    constructor(name, hp) {
+        this.name = name
+        this.hp = hp
+    }
+}
 
 function log(msg) {
     const li = document.createElement("li")
@@ -28,10 +23,16 @@ function rollDice()
     return Math.ceil(Math.random() * 6)
 }
 
+function seconds(number)
+{
+    newNumber = number * 1000
+    return newNumber
+}
+
 function updateHealth()
 {
     playerHpText.textContent = playerHp
-    enemyHpText.textContent = enemyHp
+    enemyHpText.textContent = currentEnemy.hp
 }
 
 function takeDamage(attacker, target, hp, highest_value, lowest_value)
@@ -41,6 +42,9 @@ function takeDamage(attacker, target, hp, highest_value, lowest_value)
     return new_hp
 }
 
+const enemy = new Enemy("corn", 30)
+currentEnemy = enemy
+
 function startRound()
 {
     const playerRoll = rollDice()
@@ -48,27 +52,67 @@ function startRound()
 
     if (playerRoll > enemyRoll)
     {
-       enemyHp = takeDamage(playerName, enemyName, enemyHp, playerRoll, enemyRoll)
+       currentEnemy.hp = takeDamage(playerName, currentEnemy.name, currentEnemy.hp, playerRoll, enemyRoll)
     }
     else if (playerRoll < enemyRoll)
     {
-        playerHp = takeDamage(enemyName, playerName, playerHp, enemyRoll, playerRoll)
+        playerHp = takeDamage(currentEnemy.name, playerName, playerHp, enemyRoll, playerRoll)
     }
     else
     {
         log("Alla missade sina attacker.")
     }
-    if (playerHp < 1 || enemyHp < 1)
-    {
-        if (playerHp < 1)
-        {
-            playerHp = 0
-        }
-        elseif (enemyHp < 1)
-        {
-            enemyHp = 0
-        }
-        startButton.disabled = true
-    }
     updateHealth()
 }
+
+let round
+let last = 0
+
+function gameLoop(timestamp)
+{
+    if (timestamp >= last + seconds(1))
+    {
+        startRound()
+        last = timestamp
+    }
+
+    if (playerHp <= 0)
+    {
+        stopButton.disabled = true
+        startButton.disabled = true
+        playerHp = 0
+        window.cancelAnimationFrame(round)
+        //lägg till eventuell restart knapp/function här
+    } else {
+        startButton.disabled = true
+        round = window.requestAnimationFrame(gameLoop)
+    }
+
+    if (currentEnemy.hp <= 0)
+    {
+        startButton.disabled = true
+        last += seconds(2)
+        currentEnemy = new Enemy("Förtrollad Toalett", 45)
+        startButton.disabled = false
+    }
+}
+
+function stopGame() 
+{
+    window.cancelAnimationFrame(round)
+    startButton.disabled = false
+}
+
+const startButton = document.querySelector("#start-button")
+startButton.addEventListener("click", gameLoop)
+
+const stopButton = document.querySelector("#stop-button")
+stopButton.addEventListener("click", stopGame)
+
+const playerHpText = document.querySelector("#player-hp")
+playerHpText.textContent = playerHp
+
+const enemyHpText = document.querySelector("#enemy-hp")
+enemyHpText.textContent = currentEnemy.hp
+
+const combatLog = document.querySelector("#combat-log")
