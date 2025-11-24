665b06ef9f0e7f7d8f49d423dc6292ef58b57080
Kevin Edin <kevin.edin@elev.ga.ntig.se>
2025-11-24 10:40:29 +0100

    coolare healthbars, större UI

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ class Enemy {
     constructor(name, hp) {
         this.name = name
         this.hp = hp
+        this.maxHp = hp
     }
 }
 
@@ -29,15 +30,55 @@ function seconds(number)
     return newNumber
 }
 
+function highHealth(id1, id2)
+{
+    document.getElementById(id1).src="sprites/healthbar/brawl_health.png";
+    document.getElementById(id2).src="sprites/healthbar/brawl_health_bar.png";
+}
+
+function lowHealth(id1, id2)
+{
+    document.getElementById(id1).src="sprites/healthbar/brawl_health_damaged.png";
+    document.getElementById(id2).src="sprites/healthbar/brawl_health_bar_low.png";
+}
+
 function updateHealth()
 {
-    playerHpText.textContent = playerHp
-    enemyHpText.textContent = currentEnemy.hp
+    const percentRoof = 51
+    const percentPlayer = playerHp / 100
+    const percentEnemy = currentEnemy.hp / currentEnemy.maxHp
+    const valPlayer = (percentRoof * percentPlayer).toString()
+    const valEnemy = (percentRoof * percentEnemy).toString()
+    if (valPlayer < 15.5)
+    {
+        lowHealth("player-health-bar", "player-health-bar-bar")
+    }
+    else
+    {
+        highHealth("player-health-bar", "player-health-bar-bar")
+    }
+
+    if (valEnemy < 15.5)
+    {
+        lowHealth("enemy-health-bar", "enemy-health-bar-bar")
+    }
+    else
+    {
+        highHealth("enemy-health-bar", "enemy-health-bar-bar")
+    }
+    console.log(valPlayer)
+    console.log(valEnemy)
+    document.getElementById("player-health-bar").style.width = valPlayer + "%";
+    document.getElementById("enemy-health-bar").style.width = valEnemy + "%";
 }
 
 function takeDamage(attacker, target, hp, highest_value, lowest_value)
 {
-    new_hp = hp - (highest_value - lowest_value)
+    let new_hp = hp - (highest_value - lowest_value)
+    if (new_hp < 0)
+    {
+        new_hp = 0
+    }
     log(attacker + " slog " + target + " och tog " + (hp - new_hp) + " skada.")
     return new_hp
 }
@@ -81,6 +122,7 @@ function gameLoop(timestamp)
         stopButton.disabled = true
         startButton.disabled = true
         playerHp = 0
+        updateHealth()
         window.cancelAnimationFrame(round)
         //lägg till eventuell restart knapp/function här
     } else {
@@ -91,6 +133,8 @@ function gameLoop(timestamp)
     if (currentEnemy.hp <= 0)
     {
         startButton.disabled = true
+        currentEnemy.hp = 0
+        updateHealth()
         last += seconds(2)
         currentEnemy = new Enemy("Förtrollad Toalett", 45)
         startButton.disabled = false
@@ -108,11 +152,5 @@ startButton.addEventListener("click", gameLoop)
 
 const stopButton = document.querySelector("#stop-button")
 stopButton.addEventListener("click", stopGame)
-
-const playerHpText = document.querySelector("#player-hp")
-playerHpText.textContent = playerHp
-
-const enemyHpText = document.querySelector("#enemy-hp")
-enemyHpText.textContent = currentEnemy.hp
 
 const combatLog = document.querySelector("#combat-log")
